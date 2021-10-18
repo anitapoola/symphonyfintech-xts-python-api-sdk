60a7ebc7a4f7346d627c285e46e4871af7d93d3c
anitapoola <49015957+anitapoola@users.noreply.github.com>
2021-10-18 11:11:22 +0530

    Update MarketdataSocketExample.py

--- MarketdataSocketExample.py
+++ MarketdataSocketExample.py
@@ -30,53 +30,63 @@ print("Login: ", response)
 soc = MDSocket_io(set_marketDataToken, set_muserID)
 
 # Instruments for subscribing
-Instruments = [{'exchangeSegment': 51, 'exchangeInstrumentID': 217512}]
+Instruments = [
+                {'exchangeSegment': 1, 'exchangeInstrumentID': 2885},
+                {'exchangeSegment': 1, 'exchangeInstrumentID': 26000},
+                {'exchangeSegment': 2, 'exchangeInstrumentID': 51601}
+               ]
 
 # Callback for connection
 def on_connect():
     """Connect from the socket."""
     print('Market Data Socket connected successfully!')
 
-    # Subscribe to instruments
+    # # Subscribe to instruments
+    print('Sending subscription request for Instruments - \n' + str(Instruments))
     response = xt.send_subscription(Instruments, 1501)
-    print("res: ", response)
-
+    print('Sent Subscription request!')
+    print("Subscription response: ", response)
 
 # Callback on receiving message
 def on_message(data):
     print('I received a message!')
 
+# Callback for message code 1501 FULL
+def on_message1501_json_full(data):
+    print('I received a 1501 Touchline message!' + data)
+
 # Callback for message code 1502 FULL
 def on_message1502_json_full(data):
     print('I received a 1502 Market depth message!' + data)
-    message = "1502 >> #{id} >> {data}".format(id=2885, data=data)
-    sock.send(message)
-
-# Callback for message code 1504 FULL
-def on_message1504_json_full(data):
-    print('I received a 1504 Index data message!' + data)
 
 # Callback for message code 1505 FULL
 def on_message1505_json_full(data):
     print('I received a 1505 Candle data message!' + data)
 
+# Callback for message code 1507 FULL
+def on_message1507_json_full(data):
+    print('I received a 1507 MarketStatus data message!' + data)
+
 # Callback for message code 1510 FULL
 def on_message1510_json_full(data):
     print('I received a 1510 Open interest message!' + data)
 
-# Callback for message code 1501 FULL
-def on_message1501_json_full(data):
-    print('I received a 1510 Level1,Touchline message!' + data)
-    message = "1501 >> #{id} >> {data}".format(id=2885, data=data)
-    sock.send(message)
+# Callback for message code 1512 FULL
+def on_message1512_json_full(data):
+    print('I received a 1512 Level1,LTP message!' + data)
+
+# Callback for message code 1105 FULL
+def on_message1105_json_full(data):
+    print('I received a 1105, Instrument Property Change Event message!' + data)
+
+
+# Callback for message code 1501 PARTIAL
+def on_message1501_json_partial(data):
+    print('I received a 1501, Touchline Event message!' + data)
 
 # Callback for message code 1502 PARTIAL
 def on_message1502_json_partial(data):
-    print('I received a 1502 partial message!' + data)
-
-# Callback for message code 1504 PARTIAL
-def on_message1504_json_partial(data):
-    print('I received a 1504 Index data message!' + data)
+    print('I received a 1502 Market depth message!' + data)
 
 # Callback for message code 1505 PARTIAL
 def on_message1505_json_partial(data):
@@ -86,12 +96,17 @@ def on_message1505_json_partial(data):
 def on_message1510_json_partial(data):
     print('I received a 1510 Open interest message!' + data)
 
-# Callback for message code 1501 PARTIAL
-def on_message1501_json_partial(data):
-    now = datetime.now()
-    today = now.strftime("%H:%M:%S")
-    print(today, 'in main 1501 partial Level1,Touchline message!' + data + ' \n')
-    print('I received a 1510 Level1,Touchline message!' + data)
+# Callback for message code 1512 PARTIAL
+def on_message1512_json_partial(data):
+    print('I received a 1512, LTP Event message!' + data)
+
+# Callback for message code 1507 PARTIAL
+def on_message1507_json_partial(data):
+    print('I received a 1507, NarketStatus message!' + data)
+
+# Callback for message code 1105 PARTIAL
+def on_message1105_json_partial(data):
+    print('I received a 1105, Instrument Property Change Event message!' + data)
 
 # Callback for disconnection
 def on_disconnect():
@@ -107,23 +122,26 @@ def on_error(data):
 soc.on_connect = on_connect
 soc.on_message = on_message
 soc.on_message1502_json_full = on_message1502_json_full
-soc.on_message1504_json_full = on_message1504_json_full
 soc.on_message1505_json_full = on_message1505_json_full
+soc.on_message1507_json_full = on_message1507_json_full
 soc.on_message1510_json_full = on_message1510_json_full
 soc.on_message1501_json_full = on_message1501_json_full
+soc.on_message1512_json_full = on_message1512_json_full
+soc.on_message1105_json_full = on_message1105_json_full
 soc.on_message1502_json_partial = on_message1502_json_partial
-soc.on_message1504_json_partial = on_message1504_json_partial
 soc.on_message1505_json_partial = on_message1505_json_partial
+soc.on_message1507_json_partial = on_message1507_json_partial
 soc.on_message1510_json_partial = on_message1510_json_partial
 soc.on_message1501_json_partial = on_message1501_json_partial
+soc.on_message1512_json_partial = on_message1512_json_partial
+soc.on_message1105_json_partial = on_message1105_json_partial
 soc.on_disconnect = on_disconnect
 soc.on_error = on_error
 
 # Event listener
 el = soc.get_emitter()
 el.on('Socket Connection', on_connect)
-el.on('1501-json-full', on_message1501_json_full)
-el.on('1502-json-full', on_message1502_json_full)
+
 
 # Infinite loop on the main thread. Nothing after this will run.
 # You have to use the pre-defined callbacks to manage subscriptions.
